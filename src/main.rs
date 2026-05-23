e508ca58e9afcab5d5192f4c5b027d9b006cb094
tipsypastels <dakotasankey@gmail.com>
2026-05-22 23:30:57 -0700

    finally!

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,10 @@ use objc2::{
     runtime::ProtocolObject, sel,
 };
 use objc2_app_kit::{
-    NSApplication, NSApplicationActivationPolicy, NSApplicationDelegate, NSAutoresizingMaskOptions,
-    NSBackingStoreType, NSColor, NSFont, NSMenu, NSMenuItem, NSTextAlignment, NSTextField,
-    NSWindow, NSWindowDelegate, NSWindowStyleMask,
+    NSApplication, NSApplicationDelegate, NSEventModifierFlags, NSMenu, NSMenuItem, NSStatusBar,
+    NSStatusItem,
 };
-use objc2_foundation::{
-    NSNotification, NSObject, NSObjectProtocol, NSPoint, NSRect, NSSize, ns_string,
-};
+use objc2_foundation::{NSNotification, NSObject, NSObjectProtocol, ns_string};
 use std::cell::OnceCell;
 
 fn main() {
@@ -23,8 +20,9 @@ fn main() {
     application.run();
 }
 
+#[derive(Debug, Default)]
 struct AppDelegateIvars {
-    window: OnceCell<Retained<NSWindow>>,
+    status_item: OnceCell<Retained<NSStatusItem>>,
 }
 
 define_class! {
@@ -35,115 +33,40 @@ define_class! {
 
     unsafe impl NSObjectProtocol for AppDelegate {}
     unsafe impl NSApplicationDelegate for AppDelegate {
-            // SAFETY: The signature is correct.
         #[unsafe(method(applicationDidFinishLaunching:))]
         fn did_finish_launching(&self, notification: &NSNotification) {
+            let _ = notification;
+
             let mtm = self.mtm();
+            let status_item = NSStatusBar::systemStatusBar().statusItemWithLength(-1.0);
 
-            // Retrieve the NSApplication.
-            let app = notification
-                .object()
-                .unwrap()
-                .downcast::<NSApplication>()
-                .unwrap();
-
-            // Create a minimal menubar with a "Quit" entry.
-            let menu = NSMenu::initWithTitle(NSMenu::alloc(mtm), ns_string!(""));
-            let menu_app_item = unsafe {
+            let quit_item = unsafe {
                 NSMenuItem::initWithTitle_action_keyEquivalent(
                     NSMenuItem::alloc(mtm),
-                    ns_string!(""),
-                    None,
-                    ns_string!(""),
-                )
-            };
-            let menu_app_menu = NSMenu::initWithTitle(NSMenu::alloc(mtm), ns_string!(""));
-            unsafe {
-                menu_app_menu.addItemWithTitle_action_keyEquivalent(
-                    ns_string!("Quit"),
+                    ns_string!("Quit Viscous"),
                     Some(sel!(terminate:)),
                     ns_string!("q"),
                 )
             };
-            menu_app_item.setSubmenu(Some(&menu_app_menu));
-            menu.addItem(&menu_app_item);
-            app.setMainMenu(Some(&menu));
+            quit_item.setKeyEquivalentModifierMask(NSEventModifierFlags::Command);
 
-            // Create a window and display a text field inside that.
-            let text_field = {
-                let text_field = NSTextField::labelWithString(ns_string!("Hello, World!"), mtm);
-                text_field.setFrame(NSRect::new(
-                    NSPoint::new(5.0, 100.0),
-                    NSSize::new(290.0, 100.0),
-                ));
-                text_field.setTextColor(Some(&NSColor::colorWithSRGBRed_green_blue_alpha(
-                    0.0, 0.5, 0.0, 1.0,
-                )));
-                text_field.setAlignment(NSTextAlignment::Center);
-                text_field.setFont(Some(&NSFont::systemFontOfSize(45.0)));
-                text_field.setAutoresizingMask(
-                    NSAutoresizingMaskOptions::ViewWidthSizable
-                        | NSAutoresizingMaskOptions::ViewHeightSizable,
-                );
-                text_field
-            };
+            let menu = NSMenu::new(mtm);
+            menu.setTitle(ns_string!("Viscous"));
+            menu.addItem(&quit_item);
 
-            // SAFETY: We disable releasing when closed below.
-            let window = unsafe {
-                NSWindow::initWithContentRect_styleMask_backing_defer(
-                    NSWindow::alloc(mtm),
-                    NSRect::new(NSPoint::new(0.0, 0.0), NSSize::new(300.0, 300.0)),
-                    NSWindowStyleMask::Titled
-                        | NSWindowStyleMask::Closable
-                        | NSWindowStyleMask::Miniaturizable
-                        | NSWindowStyleMask::Resizable,
-                    NSBackingStoreType::Buffered,
-                    false,
-                )
-            };
-            // SAFETY: Disable auto-release when closing windows.
-            // This is required when creating `NSWindow` outside a window
-            // controller.
-            unsafe { window.setReleasedWhenClosed(false) };
+            status_item.setMenu(Some(&menu));
 
-            // Set various window properties.
-            window.setTitle(ns_string!("A window"));
-            let view = window.contentView().expect("window must have content view");
-            view.addSubview(&text_field);
-            window.center();
-            window.setContentMinSize(NSSize::new(300.0, 300.0));
-            window.setDelegate(Some(ProtocolObject::from_ref(self)));
+            let status_button = status_item.button(mtm).unwrap();
+            status_button.setTitle(ns_string!("🌯"));
 
-            // Show the window.
-            window.makeKeyAndOrderFront(None);
-
-            // Store the window in the delegate.
-            self.ivars().window.set(window).unwrap();
-
-            app.setActivationPolicy(NSApplicationActivationPolicy::Regular);
-
-            // Activate the application.
-            // Required when launching unbundled (as is done with Cargo).
-            #[allow(deprecated)]
-            app.activateIgnoringOtherApps(true);
-        }
-    }
-
-    unsafe impl NSWindowDelegate for AppDelegate {
-        #[unsafe(method(windowWillClose:))]
-        fn window_will_close(&self, _notification: &NSNotification) {
-            // Quit the application when the window is closed.
-            let app = NSApplication::sharedApplication(self.mtm());
-            app.terminate(None);
+            self.ivars().status_item.set(status_item).unwrap();
         }
     }
 }
 
 impl AppDelegate {
     fn new(mtm: MainThreadMarker) -> Retained<Self> {
-        let this = Self::alloc(mtm).set_ivars(AppDelegateIvars {
-            window: OnceCell::new(),
-        });
+        let this = Self::alloc(mtm).set_ivars(AppDelegateIvars::default());
         unsafe { msg_send![super(this), init] }
     }
 }
